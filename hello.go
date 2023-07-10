bfb3a7d5d293da26eac14f521b926ad2386aea01
srikaanth <srikaanth62@gmail.com>
2023-07-10 10:42:13 +0530

    golang

---
+++ hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var score int = 1989
+	fmt.Println(score)
+}
---
+++ variable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var RRR string = "Tollywood"
+	fmt.Println(RRR)
+	var LAGAN string
+	LAGAN = "Bollywood"
+	fmt.Println(LAGAN)
+	AVATAR := "Hollywood"
+	fmt.Println(AVATAR)
+	// MovieRating := 9
+	// fmt.Printf("%v, %T", MovieRating, MovieRating)
+	var hero, heroine string = "Prabhas", "Kriti"
+	fmt.Println(hero, heroine)
+	var defaultValues string
+	fmt.Println(defaultValues)
+
+	var (
+		Name     = "Srikaanth"
+		Age      = 32
+		Job      = "Software Engineer"
+		Location = "Hyderabad"
+	)
+	fmt.Println(Name, Age, Job, Location)
+
+}
